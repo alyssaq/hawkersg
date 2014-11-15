224beb50049520fb235e429cd6449bc5ef65b767
Alyssa <alyssaquek@gmail.com>
2014-11-15 10:30:27 +0800

    Upgrade to express 4 with cors

--- web.js
+++ web.js
@@ -1,7 +1,22 @@
-var express = require('express'),
-	fs      = require('fs');
-	
+var express = require('express');
+var mongoose = require('mongoose');
+var cors = require('cors');
+var bodyParser = require('body-parser');
+var morgan  = require('morgan');
+var compression = require('compression');
+var fs  = require('fs')
 var app = express();
+
+app.use(bodyParser.json());
+app.use(morgan('combined'));
+app.use(compression());
+app.use(cors({
+  origin: '*',
+  methods: ['GET, PUT, POST, DELETE, OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
+
+
 app.set('views', __dirname + '/views');
 app.engine('html', require('ejs').renderFile);
 app.use("/css", express.static(__dirname + '/css'));
